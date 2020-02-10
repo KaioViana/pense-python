1260dcffb26f7f27eab57c82ddcb6aef356a65f8
KaioViana <kaiu.viana@hotmail.com>
2020-02-10 15:37:46 -0300

    teorema de Fermat adicionado

---
+++ condicionais_recursividade/fermat_teorema.py
@@ -0,0 +1,48 @@
+def check_fermat(a=0, b=0, c=0, n=0, conj=set(), tentativas=100):
+    from random import randint
+
+    print('tentativas:', tentativas, 'comprimento do conjunto:', len(conj))
+    
+    if tentativas == 0:
+        print(str('Máximo de tentativas atingido!').upper())
+        return None
+
+    a = randint(1, 10000)
+    b = randint(1, 10000)
+    c = randint(1, 10000)
+    n = randint(1, 10000)
+
+    tupl = (a,b,c,n)
+    
+    print(f'\nCÁLCULO:\nAB: {(a**2) + (b**2)} ==> C:{c**2}')
+    print('IGUALDADE: ', (a**n) + (b**n) == c**n, '\n')
+
+    if (a**n) + (b**n) == c**n and n > 2:
+        print(str('Holy Smokes, Fermat was wrong!').upper())
+        return None
+    else:
+        conj.update([tupl])
+
+        while tupl in conj:
+            a = randint(1, 10000)
+            b = randint(1, 10000)
+            c = randint(1, 10000)
+            n = randint(1, 10000)
+
+            tupl = (a,b,c,n)
+        #input(str(f'Tupla {tupl} não contém no conjunto. ENTER para continuar: \n'))
+
+        a = tupl[0]
+        b = tupl[1]
+        c = tupl[2]
+        n = tupl[3]
+        
+        check_fermat(a, b, c, n, conj, (tentativas - 1))
+
+
+def main():
+    print(check_fermat())
+
+
+if __name__ == '__main__':
+    main()
