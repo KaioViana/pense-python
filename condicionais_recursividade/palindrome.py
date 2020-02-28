f28b2bfb8b75a3de517d12d414e4e58adc56b8d1
KaioViana <kaiu.viana@hotmail.com>
2020-02-28 13:39:37 -0300

    initial commit

---
+++ condicionais_recursividade/palindrome.py
@@ -0,0 +1,22 @@
+def is_palindrome(s):
+    """Verifica se uma palavra é um palídromo.
+    :param s: palavra
+    :return: boolean
+    """
+    if len(s) <= 1:
+        return True
+    if s[0] == s[-1]:
+        return is_palindrome(s[1:-1])
+    else: return False
+
+
+def main():
+    try:
+        s = str(input('Informe uma palavra: ')).strip()
+        print(is_palindrome(s))
+    except Exception as error:
+        print('Exceção encontrada:', error)
+
+ 
+if __name__ == '__main__':
+    main()
