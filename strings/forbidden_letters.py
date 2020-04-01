99bb1c6bbae056188ec08cc362122b3b54970b8a
KaioViana <kaiu.viana@hotmail.com>
2020-04-01 17:04:40 -0300

    initial cimmit

---
+++ strings/forbidden_letters.py
@@ -0,0 +1,37 @@
+def avoids(word, letters):
+    for letter in word:
+        if letter in letters:
+            return False
+    return True
+        
+
+
+def main():
+    from time import sleep
+
+    while True:
+        user_word = str(input('INFORME UMA PALAVRA:\n')).upper().strip()
+        user_letters = str(input('INFORME AS LETRAS PROÍBIDAS:\n')).upper().strip()
+
+        print('='*21)
+        print('PROCESSANDO...')
+        sleep(1)
+        print('='*21)
+
+        if avoids(user_word, user_letters):
+            print('PASSOU NA VERIFICAÇÃO!')
+        else:
+            print('CONTÊM LETRAS PROÍBIDAS!')
+        print('='*21)
+        flag = str(input('DESEJA CONTINUAR ? (S/N)\n')).upper().strip()
+        print(flag)
+
+        while flag != 'S' and flag != 'N':
+            flag = str(input('COMANDO NÃO ENTENDIDO. INFORME NOVAMENTE: (S/N)\n')).upper().strip()
+        
+        if flag == 'N':
+            break
+        print('='*21)
+
+if __name__ == '__main__':
+    main()
